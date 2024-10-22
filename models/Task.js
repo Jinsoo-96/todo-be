1fee13956617e9b54576433b2017fad09dc92be5
신진수 <jins@sinjinsuui-Macmini.local>
2024-10-22 16:38:25 +0900

    작성자 추가하기

--- models/Task.js
+++ models/Task.js
@@ -11,9 +11,14 @@ const taskSchema = Schema(
       type: Boolean,
       required: true,
     },
+    author: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
   { timestamps: true } //몽구스에서 제공하는 옵션
-);
+); // Schema.Types.ObjectId 몽구스에서 제공하는 데이터 타입
 
 const Task = mongoose.model("Task", taskSchema);
 
--- routes/task.api.js
+++ routes/task.api.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const taskController = require("../controller/task.controller");
+const authController = require("../controller/auth.controller");
 const router = express.Router();
 
 // 모든 할 일의 isComplete 상태를 토글하는 API
 router.put("/toggle-all", taskController.toggleAllTasks);
 
-router.post("/", taskController.creatTask);
+router.post("/", authController.authenticate, taskController.creatTask);
 
 router.get("/", taskController.getTask);
 
